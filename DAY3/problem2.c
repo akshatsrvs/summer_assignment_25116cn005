6e143c49140972748929db3b39a247f47f06d617
akshatsrvs <akshat.discover@gmail.com>
2026-06-03 21:01:59 +0530

    problem2.c

--- DAY3/problem2.c
+++ DAY3/problem2.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-
-int main() {
-    int start, end, i, j, isPrime;
-
-    printf("Enter the starting number: ");
-    scanf("%d", &start);
-
-    printf("Enter the ending number: ");
-    scanf("%d", &end);
-
-    printf("Prime numbers between %d and %d are:\n", start, end);
-
-    for (i = start; i <= end; i++) {
-        if (i < 2)
-            continue;
-
-        isPrime = 1;
-
-        for (j = 2; j <= i / 2; j++) {
-            if (i % j == 0) {
-                isPrime = 0;
-                break;
+int main(){
+    int n,start,add=0,end,a,notprime;
+    printf("Enter the range Starting Number : ");
+    scanf("%d",&start);
+    printf("Enter the range Ending Number : ");
+    scanf("%d",&end);
+    a=(end+1)-start;
+    int arr[a];
+    for(int i=start;i<=end;i++){
+        for(int j=2;j<i;j++){
+            if(i!=0 && i!=1){
+                if(i%j==0){
+                    notprime=i;
+                }    
             }
         }
+        if(i!=notprime){
+            arr[add]=i;
+            add++;
 
-        if (isPrime)
-            printf("%d ", i);
+        }
+        
     }
-
+    printf("All prime numbers in the range are : ");
+    for(int z=0;z<add;z++){
+        printf("%d",arr[z]);
+        if(z!=add-1){
+            printf(", ");
+        }
+    }
     return 0;
 }
