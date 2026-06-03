94ba57b1fe4f9b5a3de903641c0ea4211dc0e8f5
akshatsrvs <akshat.discover@gmail.com>
2026-06-03 21:02:57 +0530

    problem3.c

--- DAY3/problem3.c
+++ DAY3/problem3.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-
-int main() {
-    int a, b, temp;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    while (b != 0) {
-        temp = b;
-        b = a % b;
-        a = temp;
+int main(){
+    int n,m,first,GCD,second;
+    printf("Enter the First Number : ");
+    scanf("%d",&first);
+    printf("Enter the Second Number : ");
+    scanf("%d",&second);
+    if(first>second){
+        n=first;
+        m=second;
     }
-
-    printf("GCD = %d\n", a);
-
+    else if(first<second){
+        n=second;
+        m=first;
+    }
+    else{
+        n=first;
+        m=second;
+    }
+    for(int i=1;i<n;i++){
+        if(n%i==0 && m%i==0){
+            GCD=i;
+        }
+    }
+    printf("GCD of the First Number %d and Second Number %d are : %d",first,second,GCD);
     return 0;
 }
